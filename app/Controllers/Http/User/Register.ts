28d0203c2ce03eab7f762b9688696bc4dd9646af
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-27 11:45:12 -0300

    ✉️ feat: add the mail sender on the signup controller

--- app/Controllers/Http/User/Register.ts
+++ app/Controllers/Http/User/Register.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker'
 
 import User from '../../../Models/User'
 import StoreValidator from '../../../Validators/User/StoreValidator'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class RegistersController {
   public async store({ request, auth }: HttpContextContract) {
@@ -10,13 +11,20 @@ export default class RegistersController {
     const user = await User.create({ email })
     await user.save()
 
-    const key = faker.datatype.uuid() + user.id
+    const key = faker.datatype.uuid() + new Date().getTime()
 
     user.related('keys').create({
       key,
     })
 
     const link = `${redirectUrl}/${key}}`
+
+    await Mail.send((message) => {
+      message.to(email)
+      message.from('contato@adonisbook.com', 'AdonisBook')
+      message.subject('Criação de conta')
+      message.htmlView('email/verify-email', { link })
+    })
   }
   public async show({}: HttpContextContract) {}
   public async update({}: HttpContextContract) {}
