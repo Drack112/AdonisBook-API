a6172b86aba4dfff3b954a306b9a70ac4ee31455
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-28 00:07:14 -0300

    ⏫ feat: main controller for update user

--- start/routes/user.ts
+++ start/routes/user.ts
@@ -3,5 +3,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.post('/users/register', 'User/Register.store')
 Route.get('/users/register/:key', 'User/Register.show')
 Route.put('/users/register', 'User/Register.update')
+
 Route.post('/users/forgot-password', 'User/ForgotPassword.store')
 Route.put('/users/forgot-password', 'User/ForgotPassword.update')
+
+Route.get('/users', 'User/Main.show').middleware('auth')
+Route.put('/users', 'User/Main.update').middleware('auth')
